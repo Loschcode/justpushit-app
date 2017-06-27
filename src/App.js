54096c5037861d6dc2299115221089a591d04795
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-27 17:46:49 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
@@ -11,7 +11,8 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input type="text"></input>
+          <button type="submit">Join</button>
         </p>
       </div>
     );
