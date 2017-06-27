c9310476f4b560b1af8efe781ec3308df12415bd
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-06-27 18:29:58 +0200

    test prod

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -86,7 +86,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -116,7 +116,7 @@ module.exports = {
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -134,6 +134,8 @@ module.exports = {
       // In production, they would get copied to the `build` folder.
       {
         exclude: [
+          /\.sass$/,
+          /\.scss$/,
           /\.html$/,
           /\.(js|jsx)$/,
           /\.css$/,
@@ -165,12 +167,18 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
+
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
           cacheDirectory: true,
         },
+      },
+
+      {
+        test: /\.sass$/,
+        include: paths.appSrc,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       },
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
